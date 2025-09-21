ee35575a2cd28d6792fda4b399956dce74cd4ef1
Vdv26 <vaishnavdv26@gmail.com>
2025-09-21 18:15:59 +0530

    Update config.py

--- nlp_service_aggregator/config.py
+++ nlp_service_aggregator/config.py
@@ -8,6 +8,9 @@ REDDIT_USER_AGENT = "ocean-hazard-monitor/0.1 by Beautiful-Tough-4384"
 # NewsAPI.org Credentials
 NEWS_API_KEY = "04637f1c3f96493896b30b3002f2d3c1"
 
+# Gemini API Credentials
+GEMINI_API_KEY = "AIzaSyDWb5A-os8KwsTfvhmx-Vk3hvwcHCWxCAA"
+
 # --- NLP Keywords (Shared across all collectors) ---
 HAZARD_KEYWORDS = {
     "tsunami": ["tsunami", "tidal wave", "harbor wave", "sea receding"],
